38fe34b8d669909687d6f23c2c5538079a4334b7
alexritzmann <alexritzmann@gmail.com>
2025-07-31 14:32:59 -0300

    M02S09 Ex5

--- Trello/M02S09/meu-app-react/src/components/form/Form.jsx
+++ Trello/M02S09/meu-app-react/src/components/form/Form.jsx
@@ -1,6 +1,7 @@
 
 
 import { useState } from 'react'
+import { ToastContainer, toast } from "react-toastify";
 import './Form.css'
 
 function Form(){
@@ -9,16 +10,39 @@ function Form(){
     const [postDescription, setPostDescription] = useState('')
     const [postURLCover, setPostURLCover] = useState('')
     const [postDate, setPostDate] = useState('')
-    const [postType, setPostType] = useState('')
+    const [postCategory, setPostCategory] = useState('')
+
 
     function savePost(event){
         event.preventDefault()
+
+
+        if(!postTitle || !postDescription || !postURLCover || !postDate || !postCategory){
+            toast.error('Preencha todos os campos!')
+            return
+        }
+
+        if(!postURLCover.startsWith('http') && !postURLCover.startsWith('https')) {
+            toast.error('A URL deve começar com http ou https')
+            return
+        }
+        
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const selectedDate = new Date(postDate);
+        
+        if(selectedDate < today) {
+            toast.info('Não é possível selecionar uma data passada!');
+            return;
+        }
+
+        
         alert('Post cadastrado com sucesso!')
         setPostTitle('')
         setPostDescription('')
         setPostURLCover('')
         setPostDate('')
-        setPostType('')
+        setPostCategory('')
     }
 
     return (
@@ -26,20 +50,20 @@ function Form(){
         <div className='container-form'>
             <h3>Novo Post</h3>
             <form className='form' onSubmit={savePost}>
-                <label htmlFor='postTitle'>Título:</label>
-                <input type="text" id="postTitle" placeholder='Titulo' value={postTitle} onChange={(event) => setPostTitle(event.target.value)} />
+                <label className='required-field' htmlFor='postTitle'>Título:</label>
+                <input type="text" id="postTitle" placeholder='Titulo' value={postTitle} onChange={(event) => setPostTitle(event.target.value)} required/>
 
-                <label htmlFor='postDescription'>Descricão:</label>
-                <textarea id="postDescription" placeholder='Descricão' value={postDescription} onChange={(event) => setPostDescription(event.target.value)} />
+                <label className='required-field' htmlFor='postDescription'>Descricão:</label>
+                <textarea id="postDescription" placeholder='Descricão' value={postDescription} onChange={(event) => setPostDescription(event.target.value)} required/>
 
-                <label htmlFor='postURLCover'>URL da capa:</label>
-                <input type="text" id="postURLCover" placeholder='URL da imagem da Capa' value={postURLCover} onChange={(event) => setPostURLCover(event.target.value)} />
+                <label className='required-field' htmlFor='postURLCover'>URL da capa:</label>
+                <input type="text" id="postURLCover" placeholder='URL da imagem da Capa' value={postURLCover} onChange={(event) => setPostURLCover(event.target.value)} required/>
 
-                <label htmlFor='postDate'>Data de publicação:</label>
-                <input type="date" id="postDate" value={postDate} onChange={(event) => setPostDate(event.target.value)} />
+                <label className='required-field' htmlFor='postDate'>Data de publicação:</label>
+                <input type="date" id="postDate" value={postDate} onChange={(event) => setPostDate(event.target.value)} min={new Date().toISOString().split('T')[0]}required/>
 
-                <label htmlFor='postType'>Tipodo post</label>
-                <select id="postType" value={postType} onChange={(event) => setPostType(event.target.value)}>
+                <label className='required-field' htmlFor='postCategory'>Tipo do post</label>
+                <select id="postCategory" value={postCategory} onChange={(event) => setPostCategory(event.target.value)} required>
                     <option value="Artigo">Artigo</option>
                     <option value="Noticia">Notícia</option>
                     <option value="Tutorial">Tutorial</option>
@@ -48,6 +72,7 @@ function Form(){
 
                 <button type='submit'>Cadastrar</button>
             </form>
+            <ToastContainer />
         </div>
     )
 }
